ff6e5e12ef618ff379592566288fb1de89f273f6
Jihoon Oh <jihoonoh123@gmail.com>
2017-02-13 20:47:15 -0500

    finished about page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
-$(function() {
-  // Your custom JavaScript goes here
+$('.about-scroll').click(function() {
+	var height = $('.about-landing').height() + 100;
+	$('html, body').animate({scrollTop : height}, 800);
+	return false;
 });
