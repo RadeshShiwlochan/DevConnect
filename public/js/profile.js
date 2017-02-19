804523119c8272e3f2331e893545d2056a6a3693
jwu130 <jwu3725@gmail.com>
2017-02-19 02:02:41 -0500

    Google Chart donuts

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,16 @@
 // ========
 var User = require('../../models/User');
 
-module.exports = {
-  giveBadge: function () {
-    var first_badge = new Badge({ name: 'small', points: 0, points_required: 5, category: 'java' });
+$('#giveBadge').click(function() {
+	console.log("CALLING GIVE BADGE FUNCTION");
 
-	User.findByIdAndUpdate("58a0ac69e030e21d20739a0f", {$push: {badges: first_badge}}, {safe: true, upsert: true},
-	    function(err, model) {
-	        console.log("giveBadge db error: " + err );
-    });
-  }
-};
+	var first_badge = new Badge({ name: 'small', points: 0, points_required: 5, category: 'java' });
+
+	User.findByIdAndUpdate("58a0ac69e030e21d20739a0f", 
+		{$push: {badges: first_badge}}, {safe: true, upsert: true},
+    	function(err, model) {
+        	console.log("giveBadge db error: " + err );
+		}
+	);
+
+});
