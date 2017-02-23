f69f0ea294d79b865970607f551f86079cc26c91
Jihoon Oh <jihoonoh123@gmail.com>
2017-02-23 00:12:45 -0500

    mongodb connection

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ require('./config/passport');
 var app = express();
 
 
-mongoose.connect(process.env.MONGODB);
-mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
+// mongoose.connect(process.env.MONGODB);
+// mongoose.connection.on('error', function() {
+//   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+//   process.exit(1);
+// });
 
 var hbs = exphbs.create({
   defaultLayout: 'main',
